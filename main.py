1e7e853c1eb034c7eca52ff001c4236bfe0017be
Martin Levesque <levesque.martin@gmail.com>
2023-07-04 21:15:58 -0400

    wip

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sqlite3
 
 print(f"sqlite version: {sqlite3.version}")
 
+# todo, parse chunks:
+# PRAGMA|CREATE TABLE|DROP TABLE|ALTER TABLE|CREATE INDEX|DROP INDEX ... ; (line ends with ;)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.

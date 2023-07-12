1c26af8f9e0c964a8226f9d5cccef516de65d116
Martin Levesque <levesque.martin@gmail.com>
2023-07-11 20:57:25 -0400

    minimal pragma tests

---
+++ tests/schema/test_pragma_schema.py
@@ -0,0 +1,20 @@
+from schema.pragma_schema import PragmaSchema
+
+
+def test_pragma_schema_variable_name_happy_path():
+    pragma = PragmaSchema(statement="PRAGMA journal_mode = MEMORY;", base_instruction="PRAGMA")
+
+    assert pragma.variable_name() == "journal_mode"
+
+
+def test_pragma_schema_value_happy_path():
+    pragma = PragmaSchema(statement="PRAGMA journal_mode = MEMORY;", base_instruction="PRAGMA")
+
+    assert pragma.value() == "MEMORY"
+
+
+def test_pragma_schema_value_with_override():
+    pragma = PragmaSchema(statement="PRAGMA journal_mode = MEMORY;", base_instruction="PRAGMA")
+    pragma.override_value = "WAL"
+
+    assert pragma.value() == "WAL"

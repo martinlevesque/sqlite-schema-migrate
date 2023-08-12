f9735023ebf3b4e3a003a396a372d58892bf8705
Martin Levesque <levesque.martin@gmail.com>
2023-08-12 10:00:53 -0400

    black

--- args.py
+++ args.py
@@ -1,9 +1,12 @@
 import argparse
 
 parser = argparse.ArgumentParser(
-    prog='sqlite-schema-migration', description='Migrate an sqlite3 database from a file schema')
-parser.add_argument('database', metavar='database', type=str,
-                    help='the database file to migrate')
-parser.add_argument('-f', '--force', action='store_true')
+    prog="sqlite-schema-migration",
+    description="Migrate an sqlite3 database from a file schema",
+)
+parser.add_argument(
+    "database", metavar="database", type=str, help="the database file to migrate"
+)
+parser.add_argument("-f", "--force", action="store_true")
 
 parsed_args = parser.parse_args()
